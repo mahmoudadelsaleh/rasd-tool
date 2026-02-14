4cb69dea2c3820c3c8487bc83de00d63327ce365
Mahmoud Adel Saleh <mahmoudadelsaleh@gmail.com>
2026-02-15 00:07:53 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,11 @@
-// firebase-config.js
-const firebaseConfig = { 
-    apiKey: "AIzaSyBePGZiqMPmZQdg36jZ4Rkdtd7OISxmob8", 
-    projectId: "rasd-tools" 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBePGZiqMPmZQdg36jZ4Rkdtd7OISxmob8",
+  authDomain: "rasd-tools.firebaseapp.com",
+  databaseURL: "https://rasd-tools-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "rasd-tools",
+  storageBucket: "rasd-tools.firebasestorage.app",
+  messagingSenderId: "1048930814102",
+  appId: "1:1048930814102:web:6382a0efde498149281ab7",
+  measurementId: "G-YJFDM9699T"
 };
